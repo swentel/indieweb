72f337bf640317b9f70a9b64f6b6990e0451c5e8
swentel <swentel@realize.be>
2019-11-09 12:42:42 +0100

    Issue #440 - rss - use parent::render

--- modules/indieweb_websub/src/Plugin/views/style/WebSub.php
+++ modules/indieweb_websub/src/Plugin/views/style/WebSub.php
@@ -21,44 +21,15 @@ use Drupal\views\Plugin\views\style\Rss;
 class WebSub extends Rss {
 
   public function render() {
-    if (empty($this->view->rowPlugin)) {
-      trigger_error('Drupal\views\Plugin\views\style\Rss: Missing row plugin', E_WARNING);
-      return [];
-    }
-    $rows = [];
+    $build = parent::render();
 
-    // This will be filled in by the row plugin and is used later on in the
-    // theming output.
-    $this->namespaces = ['xmlns:dc' => 'http://purl.org/dc/elements/1.1/'];
-
-    // Fetch any additional elements for the channel and merge in their
-    // namespaces.
-    $this->channel_elements = $this->getChannelElements();
-    foreach ($this->channel_elements as $element) {
-      if (isset($element['namespace'])) {
-        $this->namespaces = array_merge($this->namespaces, $element['namespace']);
-      }
-    }
-
-    foreach ($this->view->result as $row_index => $row) {
-      $this->view->row_index = $row_index;
-      $rows[] = $this->view->rowPlugin->render($row);
-    }
-
+    // Will only work if views caching is disabled, but we'll fix that later.
     $config = \Drupal::config('indieweb_websub.settings');
     $links = [
       '<' . $config->get('hub_endpoint') . '>; rel="hub"',
       '<' . Url::fromUri('internal:/' . $this->view->getPath(), ['absolute' => TRUE])->toString() . '>; rel="self"',
     ];
     $this->view->getResponse()->headers->set('link', $links);
-
-    $build = [
-      '#theme' => $this->themeFunctions(),
-      '#view' => $this->view,
-      '#options' => $this->options,
-      '#rows' => $rows,
-    ];
-    unset($this->view->row_index);
 
     return $build;
   }
