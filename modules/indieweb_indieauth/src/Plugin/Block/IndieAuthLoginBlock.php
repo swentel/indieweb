6426060575aea6908b448340e1f457d88b22794b
swentel <swentel@realize.be>
2020-01-06 16:55:04 +0100

    Issue by eelkeblok: fix fatal in IndieAuth login block if external authentication is not enabled

--- modules/indieweb_indieauth/src/Plugin/Block/IndieAuthLoginBlock.php
+++ modules/indieweb_indieauth/src/Plugin/Block/IndieAuthLoginBlock.php
@@ -5,6 +5,7 @@ namespace Drupal\indieweb_indieauth\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Url;
@@ -18,27 +19,45 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   admin_label = @Translation("Web sign-in"),
  * )
  */
-class IndieAuthLoginBlock extends BlockBase {
+class IndieAuthLoginBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Set the form builder.
+   *
+   * @param \Drupal\Core\Form\FormBuilderInterface $formBuilder
+   *   The form builder (e.g. from the container).
+   */
+  public function setFormBuilder(FormBuilderInterface $formBuilder) {
+    $this->formBuilder = $formBuilder;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $render_form = TRUE;
+    $build = [
+      'form' => $this->formBuilder->getForm('Drupal\indieweb_indieauth\Form\IndieAuthLoginForm'),
+    ];
 
-    if (\Drupal::currentUser()->isAuthenticated()) {
-      /** @var \Drupal\externalauth\AuthmapInterface $external_authmap */
-      $external_authmap = \Drupal::service('externalauth.authmap');
-      if ($external_authmap && $external_authmap->get(\Drupal::currentUser()->id(), 'indieweb')) {
-        $render_form = FALSE;
-      }
-    }
-
-    if ($render_form) {
-      $build['form'] = \Drupal::formBuilder()->getForm('Drupal\indieweb_indieauth\Form\IndieAuthLoginForm');
-    }
     return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    /** @var \Drupal\indieweb_indieauth\Plugin\Block\IndieAuthLoginBlock $block */
+    $block = new static($configuration, $plugin_id, $plugin_definition);
+    $block->setFormBuilder($container->get('form_builder'));
+
+    return $block;
   }
 
 }
